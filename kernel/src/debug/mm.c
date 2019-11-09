6ed98a38f618849ad64585925a553b44591001a4
Tomáš Drozdík <drozdik.tomas@gmail.com>
2019-11-09 17:09:16 +0100

    Implement get_base_memory_size function

--- kernel/src/debug/mm.c
+++ kernel/src/debug/mm.c
@@ -12,5 +12,21 @@
  * @return Amount of memory available in bytes.
  */
 size_t debug_get_base_memory_size(void) {
-    return 0;
+    // Go through the memory try to assign something then read it.
+    // If the read value is what we've assigned before then we consider this a
+    // valid memory block.
+    volatile uintptr_t* addr = (uintptr_t*)&_kernel_end;
+    size_t memory_size = 0;
+
+    while (true) {
+        ++addr;
+        // This is why addr is volatile, if it weren't compiler would assume
+        // that what we assigned must be there.
+        *addr = (uintptr_t)addr;
+        if (*addr != (uintptr_t)addr) {
+            break;
+        }
+        memory_size += sizeof(addr);
+    }
+    return memory_size;
 }
