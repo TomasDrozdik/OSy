977a35fb2620d1686fe955d7a639ac0e0d842d9e
Tomáš Drozdík <drozdik.tomas@gmail.com>
2020-02-17 13:43:34 +0100

    Remove userspace main manual msim breakpoint.

--- userspace/example/main.c
+++ userspace/example/main.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 int main(void) {
-    __asm__ volatile(".insn\n\n.word 0x29\n\n");
+    //__asm__ volatile(".insn\n\n.word 0x29\n\n");
     puts("[APPL]: Hello from userspace!");
 
     return 0;
