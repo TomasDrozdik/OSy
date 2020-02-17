928215cb70c84636b4c699a2cdbc846ceeeda785
Tomáš Drozdík <drozdik.tomas@gmail.com>
2020-02-17 13:40:22 +0100

    Clarify exception processing.

--- kernel/src/exc/exc.c
+++ kernel/src/exc/exc.c
@@ -8,30 +8,55 @@
 #include <proc/scheduler.h>
 #include <proc/userspace.h>
 
-/* General exception code value mnomonics. */
-#define Int 0 /* Interrupt. */
-#define TLBL 2 /* TLB exception (load or instruction fetch). */
-#define TLBS 3 /* TLB exception (store). */
-#define SYS 8 /* Syscall. */
+/* General exception code value mnemonics. */
+#define Int  0  /* Interrupt. */
+#define TLBL 2  /* TLB exception (load or instruction fetch). */
+#define TLBS 3  /* TLB exception (store). */
+#define AdEL 4  /* Address error exception (load or instruction fetch). */
+#define Sys  8  /* Syscall. */
+#define CpU  11 /* Coprocessor unusable exception. */
+
+/* Interrupt code mnemonics. */
+#define CLK  7  /* Clock interrupt. */
+
+static void handle_interrupt(context_t* context) {
+    if (cp0_cause_is_interrupt_pending(context->cause, CLK)) {
+        timer_interrupt_after(CYCLES);
+        scheduler_schedule_next();
+    }
+}
 
 void handle_exception_general(context_t* context) {
     unative_t exc = cp0_cause_get_exc_code(context->cause);
     switch (exc) {
     case Int:
-        if (cp0_cause_is_interrupt_pending(context->cause, 7)) {
-            timer_interrupt_after(CYCLES);
-            scheduler_schedule_next();
-        }
+        dprintk("Interrupt.. cause:%u, status: %x, epc: %x\n",
+                context->cause, context->status, context->epc);
+        handle_interrupt(context);
+        return;
+    case AdEL:
+        dprintk("Address error exception (load or instruction fetch).. status: %x, epc: %x\n",
+                context->status, context->epc);
+        thread_kill(thread_get_current());
         return;
     case TLBL:
+        dprintk("TLB exception (load or instruction fetch).. status: %x, epc: %x\n",
+                context->status, context->epc);
+        thread_kill(thread_get_current());
+        return;
     case TLBS:
         dprintk("TLBL / TLBS exception -> killing current thread.\n");
         thread_kill(thread_get_current());
         return;
-    case SYS:
-        //dprintk("Syscall was called.\n");
+    case Sys:
+        dprintk("Syscall.. status: %x, epc: %x\n",
+                context->status, context->epc);
         handle_syscall(context);
-        //dprintk("Syscall successfull,returning.....\n");
+        return;
+    case CpU:
+        dprintk("Coprocessor unsusable exception.. status: %x, epc: %x\n",
+                context->status, context->epc);
+        thread_kill(thread_get_current());
         return;
     default:
         panic("Exception...%d, status: %x, epc: %x\n", exc, context->status, context->epc);
